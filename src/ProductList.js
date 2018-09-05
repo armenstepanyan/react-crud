60eb4a1d073c827b509b850cab09efb2c35c0d94
Armen Stepanyan <st.armen91@gmail.com>
2018-09-05 13:32:30 +0300

    Initial commit

---
+++ src/ProductList.js
@@ -0,0 +1,48 @@
+import React, {Component} from 'react';
+import PropTypes from 'prop-types';
+import products from './mockData';
+
+class ProductList extends Component {
+
+    constructor(props){
+        super(props);
+        this.state = {products: []};
+    }
+
+    componentDidMount(){
+        console.log('componentDidMount');
+       setTimeout(() =>  this.setState({products: products}), 1000);
+
+
+    }
+
+    render() {
+
+        const tbody = this.state.products.map((item, index) => <tr key={index}>
+            <td>{item.id}</td>
+            <td>{item.title}</td>
+            <td>{item.text}</td>
+        </tr>);
+        return (
+            <div className='container'>
+                <table className='table'>
+                    <thead>
+                    <tr>
+                        <th>ID</th>
+                        <th>Name</th>
+                        <th>Description</th>
+                    </tr>
+                    </thead>
+                    <tbody>
+                    {tbody}
+                    </tbody>
+                </table>
+            </div>
+        )
+
+    }
+
+}
+
+export default ProductList;
+
---
+++ src/mockData.js
@@ -0,0 +1,25 @@
+const products = [
+    {
+        id: 1,
+        title: 'Test name',
+        text: 'Lorem Ipsum is simply dummy text of the printing and typesetting industry. Lorem Ipsum has been the industry\'s standard dummy text ever since the 1500s, when an unknown printer took a galley of type and scrambled it to make a type specimen book. It has survived not only five centuries, but also the leap into electronic typesetting, remaining essentially unchanged. It was popularised in the 1960s with the release of Letraset sheets containing Lorem Ipsum passages, and more recently with desktop publishing software like Aldus PageMaker including versions of Lorem Ipsum.'
+    },
+    {
+        id: 2,
+        title: ' Intro to React',
+        text: 'We will build a small game during this tutorial. You might be tempted to skip it because you’re not building games — but give it a chance. The techniques you’ll learn in the tutorial are fundamental to building any React apps, and mastering it will give you a deep understanding of React'
+    },
+    {
+        id: 3,
+        title: 'React-Bootstrap',
+        text: 'The most popular front-end framework, rebuilt for React.'
+    },
+    {
+        id: 4,
+        title: 'Angular',
+        text: 'Build features quickly with simple, declarative templates. Extend the template language with your own components and use a wide array of existing components. Get immediate Angular-specific help and feedback with nearly every IDE and editor. All this comes together so you can focus on building amazing apps rather than trying to make the code work.'
+    }
+
+];
+
+export default  products;
